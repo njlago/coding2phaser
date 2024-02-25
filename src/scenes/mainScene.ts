17dad8843af51602d7afaf8c6d994ba80d742105
njlago <nicklago7@yahoo.com>
2024-02-25 18:32:15 -0500

    firstcomm

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,28 +1,13 @@
 import Phaser from "phaser";
-import PhaserLogo from "../objects/phaserLogo";
-import FpsText from "../objects/fpsText";
 
 export default class MainScene extends Phaser.Scene {
-    fpsText: FpsText;
-
     constructor() {
         super({ key: "MainScene" });
     }
 
     create() {
-        new PhaserLogo(this, this.cameras.main.width / 2, 0);
-        this.fpsText = new FpsText(this);
-
-        const message = `Phaser v${Phaser.VERSION}`;
-        this.add
-            .text(this.cameras.main.width - 15, 15, message, {
-                color: "#000000",
-                fontSize: "24px",
-            })
-            .setOrigin(1, 0);
+        this.add.image(400, 300, "sky");
     }
 
-    update() {
-        this.fpsText.update();
-    }
+    update() {}
 }
--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -6,10 +6,17 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("phaser-logo", "assets/img/phaser-logo.png");
+        this.load.image("sky", "assets/sky.png");
+        this.load.image("ground", "assets/ground.png");
+        this.load.image("star", "assets/star.png");
+        this.load.image("bomb", "assets/bomb.png");
+        this.load.spritesheet("dude", "dude.png", {
+            frameWidth: 32,
+            frameHeight: 48,
+        });
     }
 
     create() {
-        this.scene.start("MainScene");
+        this.add.image(400, 300, "sky");
     }
 }
