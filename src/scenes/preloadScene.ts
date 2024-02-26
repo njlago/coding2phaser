745b212165cf6b0b008500ce324d5f191a5fce8c
njlago <nicklago7@yahoo.com>
2024-02-25 19:44:20 -0500

    secondcomm

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -7,16 +7,16 @@ export default class PreloadScene extends Phaser.Scene {
 
     preload() {
         this.load.image("sky", "assets/sky.png");
-        this.load.image("ground", "assets/ground.png");
+        this.load.image("platform", "assets/platform.png");
         this.load.image("star", "assets/star.png");
         this.load.image("bomb", "assets/bomb.png");
-        this.load.spritesheet("dude", "dude.png", {
+        this.load.spritesheet("dude", "assets/dude.png", {
             frameWidth: 32,
             frameHeight: 48,
         });
     }
 
     create() {
-        this.add.image(400, 300, "sky");
+        this.scene.start("MainScene");
     }
 }
